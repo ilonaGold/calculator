8293fcfe8f8aeb844daf46c9d181508ab5fda135
Ilona Goldmane <songa17@inbox.lv>
2022-01-15 12:47:26 +0200

    Added delete button and gave buttons more specific classes

---
+++ script.js
@@ -0,0 +1,37 @@
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+
+// let sum = document.getElementById("sum-el");
+
+// function add() {
+//     let result = num1 + num2;
+//     sum.textContent = "Sum: " + result;
+// }
+
+// function substract() {
+//     let result = num1 - num2;
+//     sum.textContent = "Sum: " + result;
+// }
+
+
+// function divide() {
+//     let result = num1 / num2;
+//     sum.textContent = "Sum: " + result;
+// }
+
+
+// function multiply() {
+//     let result = num1 * num2;
+//     sum.textContent = "Sum: " + result;
+// }
+
+
+const numberButtons = document.querySelectorAll(".data-number");
+const operationButtons = document.querySelectorAll(".data-operation");
+const equalsButton = document.querySelector("equals");
+// const deleteButton = document.querySelector('[data-delete]')
+// const allClearButton = document.querySelector('[data-all-clear]')
+// const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+// const currentOperandTextElement = document.querySelector('[data-current-operand]')
